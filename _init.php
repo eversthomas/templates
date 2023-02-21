c32539f5de7738671bc05619926f6984fd6b485e
eversthomas <evers.thomas@gmail.com>
2023-02-21 13:31:31 +0100

    new intit php

--- _init.php
+++ _init.php
@@ -4,3 +4,5 @@
 // This is defined by $config->prependTemplateFile in /site/config.php.
 // Use this to define shared variables, functions, classes, includes, etc. 
 
+// include automatic scss compiler
+include 'style.php';
