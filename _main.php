435f7a2aab348cf129ad1e63cf786d3300473718
eversthomas <evers.thomas@gmail.com>
2023-02-21 14:59:21 +0100

    with menu in header

--- _main.php
+++ _main.php
@@ -33,6 +33,36 @@ $home = $pages->get('/'); // homepage
 
 			</div>
 
+			<nav class="wrapper">
+				<?php $treeMenu = $modules->get("MarkupSimpleNavigation"); // load the module
+				$options = array(
+					'parent_class' => 'parent',
+					'current_class' => 'current',
+					'has_children_class' => 'has_children',
+					'levels' => true,
+					'levels_prefix' => 'level-',
+					'max_levels' => null,
+					'firstlast' => false,
+					'collapsed' => false,
+					'show_root' => true,
+					'selector' => '',
+					'selector_field' => 'nav_selector',
+					'outer_tpl' => '<ul>||</ul>',
+					'inner_tpl' => '<ul>||</ul>',
+					'list_tpl' => '<li%s>||</li>',
+					'list_field_class' => '',
+					'item_tpl' => '<a href="{url}">{title}</a>',
+					'item_current_tpl' => '<a href="{url}">{title}</a>',
+					'xtemplates' => '',
+					'xitem_tpl' => '<a href="{url}">{title}</a>',
+					'xitem_current_tpl' => '<span>{title}</span>',
+					'date_format' => 'Y/m/d',
+					'code_formatting' => false,
+					'debug' => false
+				);
+				echo $treeMenu->render($options); ?>
+			</nav>
+
 		</header>
 
 		<main class="content">
