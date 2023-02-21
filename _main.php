479c8e3812acfe52cd76262b493c6c26f313973c
eversthomas <evers.thomas@gmail.com>
2023-02-21 15:55:25 +0100

    favicon and cookie consent

--- _main.php
+++ _main.php
@@ -17,11 +17,15 @@ $home = $pages->get('/'); // homepage
 ?><!DOCTYPE html>
 <html lang="de">
 	<head id="html-head">
+		<meta charset="utf-8"/>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
 		<title><?php echo $title; ?></title>
 		<meta name="description" content="<?php echo $description; ?>"/>
 		<meta name="keywords" content="<?php echo $keywords; ?>"/>
 		<meta name="author" content="<?php echo $author; ?>" />
+
+		<link rel="shortcut icon" type="image/x-icon" href="<?php echo $config->urls->templates; ?>favicon.ico">
 
 		<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates; ?>style.css" />
 	</head>
@@ -63,6 +67,23 @@ $home = $pages->get('/'); // homepage
 	
 	<!-- scripts -->
 	<script src="<?php echo $config->urls->templates; ?>scripts/main.js"></script>
+	<script>
+		window.cookieconsent.initialise({
+		  "palette": {
+			"popup": {
+			  "background": "#eb6c44",
+			  "text": "#ffffff"
+			},
+			"button": {
+			  "background": "#f5d948"
+			}
+		  },
+		  "type": "opt-in",
+		  "content": {
+			"href": "https://www.kybernethik.org/datenschutz/"
+		  }
+		});
+	</script>
 
 	</body>
 </html>
